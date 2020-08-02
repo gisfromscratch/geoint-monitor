e8ff9cff0ae2b416cc557748f179e8bf33112143
J.T <gisfromscratch@live.de>
2020-08-02 18:15:20 +0200

    #4 Added the GdeltEventLayer class. This implementation should directly access the GDELT using the API 2.0.

---
+++ src/Solution/App/GdeltEventLayer.cpp
@@ -0,0 +1,29 @@
+// GEOINT Monitor is a sample native desktop application for geospatial intelligence workflows.
+// Copyright (C) 2020 Jan Tschada (gisfromscratch@live.de)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Additional permission under GNU LGPL version 3 section 4 and 5
+// If you modify this Program, or any covered work, by linking or combining
+// it with ArcGIS Runtime for Qt (or a modified version of that library),
+// containing parts covered by the terms of ArcGIS Runtime for Qt,
+// the licensors of this Program grant you additional permission to convey the resulting work.
+// See <https://developers.arcgis.com/qt/> for further information.
+//
+#include "GdeltEventLayer.h"
+
+GdeltEventLayer::GdeltEventLayer(QObject *parent) : QObject(parent)
+{
+
+}
