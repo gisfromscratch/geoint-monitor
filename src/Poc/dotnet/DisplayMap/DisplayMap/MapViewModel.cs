c6fbeb69068cce0523755777e212b3eba266fe94
J.T <gisfromscratch@live.de>
2020-11-15 15:02:06 +0100

    Removed unneccessay usings

--- src/Poc/dotnet/DisplayMap/DisplayMap/MapViewModel.cs
+++ src/Poc/dotnet/DisplayMap/DisplayMap/MapViewModel.cs
@@ -1,18 +1,8 @@
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Runtime.CompilerServices;
 using Esri.ArcGISRuntime.Data;
-using Esri.ArcGISRuntime.Geometry;
-using Esri.ArcGISRuntime.Location;
 using Esri.ArcGISRuntime.Mapping;
-using Esri.ArcGISRuntime.Security;
-using Esri.ArcGISRuntime.Symbology;
-using Esri.ArcGISRuntime.Tasks;
-using Esri.ArcGISRuntime.UI;
 using Esri.ArcGISRuntime.Portal;
 using System.Diagnostics;
 
